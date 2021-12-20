086db075401eedd854bb84e61a8a3676dffef41e
zahrebelnyi_vitalii <zfdhhfhh@gmail.com>
2021-12-20 11:30:05 +0200

    HW_8 unfinished

--- homeworks/workWithFiles/workWithFiles.py
+++ homeworks/workWithFiles/workWithFiles.py
@@ -1,27 +1,33 @@
 print("Calculator!")
 
 while True:
+
     i = input("Знак (+,-,*,/): ")
     if i in ('+', '-', '*', '/'):
-        x = float(input("x= "))
-        y = float(input("y= "))
+        x = (float(input("x= ")))
+        y = (float(input("y= ")))
         if i == '+':
             c = x + y
-            print(str(c))
+            print (str(c))
+            with open("result.txt", "w") as file:
+                file.write(c)
         elif i == '-':
             c1 = x - y
             print(str(c1))
+            with open("result.txt", "w") as file:
+                file.write(c1)
         elif i == '*':
             c2 = x * y
             print(str(c2))
+            with open("result.txt", "w") as file:
+                file.write(c2)
         elif i == '/':
             if y != 0:
                 c3 = x / y
                 print(str(c3))
+                with open("result.txt", "w") as file:
+                    file.write(c3)
             else:
                 print("Деление на ноль!")
     else:
         print("Неверный знак операции!")
-
-with open("result.txt", "w") as file:
-    file.write(f'{c},{c1},{c2},{c3}')
