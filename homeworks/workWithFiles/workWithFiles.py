0973219fe78a6cb47187aebd312aabc9d7e322f3
zahrebelnyi_vitalii <zfdhhfhh@gmail.com>
2021-12-27 00:18:31 +0200

    HW_8 FINISH(choice)

--- homeworks/workWithFiles/workWithFiles.py
+++ homeworks/workWithFiles/workWithFiles.py
@@ -1,33 +1,41 @@
-print("Calculator!")
-
-while True:
-
-    i = input("Знак (+,-,*,/): ")
-    if i in ('+', '-', '*', '/'):
-        x = float(input("x= "))
-        y = float(input("y= "))
-        if i == '+':
-            c = x + y
-            print(c)
-            with open("result.txt", "w") as file:
-                file.write(str(c))
-        elif i == '-':
-            c1 = x - y
-            print(c1)
-            with open("result.txt", "w") as file:
-                file.write(str(c1))
-        elif i == '*':
-            c2 = x * y
-            print(c2)
-            with open("result.txt", "w") as file:
-                file.write(str(c2))
-        elif i == '/':
-            if y != 0:
-                c3 = x / y
-                print(c3)
-                with open("result.txt", "w") as file:
-                    file.write(str(c3))
-            else:
-                print("Деление на ноль!")
-    else:
-        print("Неверный знак операции!")
+a = str(input(" "))  # value 1
+b = str(input(" "))  # Sign
+c = str(input(" "))  # value 2
+p = a + b + c
+k=eval(p)
+with open("result.txt", "w") as file:
+    file.write(str(k))
+print(k)
+# print("Calculator!")
+#
+# while True:
+#
+#     i = input("Знак (+,-,*,/): ")
+#     if i in ('+', '-', '*', '/'):
+#         x = float(input("x= "))
+#         y = float(input("y= "))
+#         if i == '+':
+#             c = x + y
+#             print(c)
+#             with open("result.txt", "w") as file:
+#                 file.write(str(c))
+#         elif i == '-':
+#             c1 = x - y
+#             print(c1)
+#             with open("result.txt", "w") as file:
+#                 file.write(str(c1))
+#         elif i == '*':
+#             c2 = x * y
+#             print(c2)
+#             with open("result.txt", "w") as file:
+#                 file.write(str(c2))
+#         elif i == '/':
+#             if y != 0:
+#                 c3 = x / y
+#                 print(c3)
+#                 with open("result.txt", "w") as file:
+#                     file.write(str(c3))
+#             else:
+#                 print("Деление на ноль!")
+#     else:
+#         print("Неверный знак операции!")
